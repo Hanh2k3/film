cd2dc82134b6c01c6c3a9fb42fae5a3cb9d63d93
Hanh <hanhlv.21it@vku.udn.vn>
2022-12-20 02:55:29 +0700

    Hanh

--- app/Http/Controllers/Client/LoginController.php
+++ app/Http/Controllers/Client/LoginController.php
@@ -75,9 +75,6 @@ class LoginController extends Controller
                 session()->put('user_name', $email->user_name);
                 session()->put('user_avatar', $email->avt);
                 session()->put('type_user', $user->type_user);
-
-                dd(session('type_user'));
-
                 session()->put('google', 'google');
                 return redirect()->route('home.');
             } else {
